881a563208f16cf6b94de14e557463856fc3263d
LucasRomero <romerolucasdaniel@outlook.com>
2023-02-22 16:40:18 -0300

    create searchComponent, adding style, and reactiveForm

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, filter, map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-search',
-  templateUrl: './search.component.html',
+  template: `
+    <div class="search">
+      <input
+        type="text"
+        class="search__input"
+        placeholder="City..."
+        [formControl]="inputSearch"
+      />
+    </div>
+  `,
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+  inputSearch = new FormControl('');
+  @Output() subtmited = new EventEmitter<string>();
 
+  ngOnInit(): void {
+    this.onChange();
+  }
+
+  private onChange(): void {
+    this.inputSearch.valueChanges
+      .pipe(
+        map((search) => search.trim()),
+        debounceTime(850),
+        distinctUntilChanged(),
+        filter((search: string) => search !== ''),
+        tap((search: string) => this.subtmited.emit(search))
+      )
+      .subscribe();
+  }
 }
