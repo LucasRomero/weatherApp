4a859f6e4058693dcb7cd49815e36fdcdd560652
LucasRomero <romerolucasdaniel@outlook.com>
2023-02-28 17:15:04 -0300

    add image to card and some styling

--- src/app/pages/weather/weather.component.ts
+++ src/app/pages/weather/weather.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { WeatherData } from '../../shared/interfaces/weather.interface';
 
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
-  styleUrls: ['./weather.component.css']
+  styleUrls: ['./weather.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class WeatherComponent {
-
+  @Input() weather!: WeatherData;
+  public BASE_URL = 'http://openweathermap.org/img/wn';
 }
